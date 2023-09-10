d652e78b6c040c3c9ccc85d11ed920862e6f9095
bartosz.rzemek@gmail.com <bartosz.rzemek@codecool.com>
2023-09-10 13:04:14 +0200

    Links

--- src/Router/About.js
+++ src/Router/About.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Contact from "./Contact";
 import Bio from "./Bio";
 
@@ -6,7 +6,11 @@ const About = () => (
     <div>
         <h1>About</h1>
 
-        <Routes>
+        <Link to={`/about`}>About</Link> | 
+        <Link to={`/about/contact`}>Contact</Link> | 
+        <Link to={`/about/bio`}>Bio</Link>
+        
+         <Routes>
             <Route path={`/contact`} element={<Contact />} />
             <Route path={`/bio`} element={<Bio />} />
         </Routes>
